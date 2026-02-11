a72592c5250191475b3567206feda732c94518af
stefa <stefa@10.3.0.18>
2026-02-11 22:20:42 +0100

    0

--- pl.manester/src/main/java/pl/manester/gui/LoginScreen.java
+++ pl.manester/src/main/java/pl/manester/gui/LoginScreen.java
@@ -1,10 +1,18 @@
 package pl.manester.gui;
 
-import javax.swing.border.StrokeBorder;
-
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
@@ -13,8 +21,11 @@ import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import pl.manester.app.SharedObjects;
+import pl.manester.gui.events.CellEventHandler;
 
 public class LoginScreen extends Application{
 	
@@ -30,7 +41,7 @@ public class LoginScreen extends Application{
 		PreparedObjects preparedObjects = sharedObjects.getPreparedObjects();
 		Border border = new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT));
 
-		primaryStage.setHeight(368);
+		primaryStage.setHeight(400);
 		primaryStage.setWidth(580);
 		primaryStage.getIcons().add(new Image(sharedObjects.getGraph().getLogoFile().toString(), false));
 
@@ -41,11 +52,51 @@ public class LoginScreen extends Application{
 		server.setPrefWidth(360);
 		server.setPrefHeight(24);
 		
+		
 		Pane pn0 = new Pane();
 		pn0.setBorder(border);
 		pn0.setPrefSize(360, 240);
 		pn0.setLayoutX(196);
 		pn0.setLayoutY(68);
+		
+		TableView<String> tableView = new TableView<>();
+		tableView.setPrefSize(358, 240);
+		TableColumn<String, String> coll = new TableColumn<String, String>();
+		
+		coll.setCellValueFactory(data -> new SimpleStringProperty(data.getValue()));
+		
+		coll.setPrefWidth(360);
+		tableView.getItems().add (new String("10.8.0.10:5432"));
+		
+		tableView.getColumns().add(coll);
+		
+		TextField textField = new TextField();
+		textField.setPrefSize(360, 24);
+		textField.setLayoutX(196);
+		textField.setLayoutY(316);
+		textField.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				tableView.getItems().add (new String(textField.getText()));
+			}
+		});
+		
+		tableView.setOnKeyPressed(new EventHandler<Event>() {
+
+			@Override
+			public void handle(Event event) {
+				// TODO Auto-generated method stub
+				if (event.toString().contains("code = DELETE")) {
+				System.out.println(event.toString());
+				}
+			}
+
+		
+		});
+		
+		pn0.getChildren().addAll(tableView);
 		
 		Pane pane = new Pane();
 		Scene scene = new Scene(pane);
@@ -54,7 +105,8 @@ public class LoginScreen extends Application{
 				preparedObjects.createTextField("login", 34, 34),
 				preparedObjects.createPasswordField("hasło", 34, 68),
 				pn0,
-				server
+				server,
+				textField
 				);
 		
 		primaryStage.setScene(scene);
